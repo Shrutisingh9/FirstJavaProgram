54a2d927d14a9863e18ada3ddb6110620138e8ba
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-26 20:32:55 +0530

    Add main.java file

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello world!");
+
+//        wap to add three numbers
+
+        int num1 = 8;
+        int num2 = 5;
+        int num3 = 7;
+
+        int sum = num1 + num2 + num3;
+
+        System.out.print("The sum of these numbers is: ");
+        System.out.println(sum);
+    }
+}
