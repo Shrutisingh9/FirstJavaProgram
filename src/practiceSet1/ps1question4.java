61dc6e79f2a54700495022e1bd8d5da4a12760bf
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-26 22:52:29 +0530

    modify ps1question4.java file

--- src/practiceSet1/ps1question4.java
+++ src/practiceSet1/ps1question4.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class ps1question4 {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         double kilometers;
 
         System.out.println("Please enter kilometers:");
-        kilometers = in.nextDouble();
+        kilometers = sc.nextDouble();
 
         double miles = kilometers / 1.609;
 
