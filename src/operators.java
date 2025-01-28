f445e7e4adfc4a59a0d12bd08eed471f5fa126ce
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-28 14:02:51 +0530

    Add operators.java file

---
+++ src/operators.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        //arithmetic operators
+        int a= 4;
+        int c = 6+a;
+        System.out.println(c);
+
+        System.out.println(4.8%1.1);
+        //assignment operator
+        int b = 9;
+        b+=3;
+
+        System.out.println(b);
+
+        //comparison operator
+        System.out.println(6 == 6);
+        System.out.println(64 > 9);
+
+        // logical operator
+        System.out.println(64 > 9 && 64 < 8 );
+        System.out.println(64 > 9 || 64 < 8 );
+
+        //bitwise operator
+        System.out.println((2 & 3));
+
+        //10
+        //11
+        //-----
+        //10
+
+
+
+
+    }
+}
