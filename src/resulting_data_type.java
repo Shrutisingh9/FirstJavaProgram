27658ad527cd1654225d20312c9ddd9205a307d6
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-28 21:07:26 +0530

    Add resulting_data_type.java file

---
+++ src/resulting_data_type.java
@@ -0,0 +1,17 @@
+public class resulting_data_type {
+    public static void main(String[] args) {
+        int a= 654 + 6;
+        float b= 6.54f + 6;
+        System.out.println(a);
+        System.out.println(b);
+
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int s = y+z;
+        float t = 6.54f +x;
+        System.out.println(s);
+        System.out.println(t);
+
+    }
+}
