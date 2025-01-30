ce15b67eb624c409290de96f1197b7d618b1ef2d
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-30 21:08:43 +0530

    Add ps2question3.java file

---
+++ src/practiceSet2/ps2question3.java
@@ -0,0 +1,17 @@
+//use comparison operators to find out whether a given number is greater than the user entered number or not
+package practiceSet2;
+import java.util.Scanner;
+public class ps2question3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter b: ");
+        int b = sc.nextInt();
+
+        System.out.println(a > b);
+    }
+}
