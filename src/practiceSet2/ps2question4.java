ddf95ea5aeda072aa141395a18b7c99b09fd4e42
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-30 21:24:58 +0530

    edit ps2question4.java file

---
+++ src/practiceSet2/ps2question4.java
@@ -0,0 +1,14 @@
+//write the following expression in a java program:v^2-u^2/2as
+package practiceSet2;
+
+public class ps2question4 {
+    public static void main(String[] args) {
+        int a = 2;
+        int v = 30;
+        int u = 20;
+        int s = 3;
+        int r= (v*v - u*u)/(2*a*s);
+        System.out.println(r);
+    }
+
+}
