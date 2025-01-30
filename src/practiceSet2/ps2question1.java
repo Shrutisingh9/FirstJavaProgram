d4d2521e6ab459a2a1f9f011765c911d472cbb27
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-30 20:44:54 +0530

    Add ps2question1.java file

---
+++ src/practiceSet2/ps2question1.java
@@ -0,0 +1,9 @@
+//what will be the result of the following expression float a = 7/4*9/2
+package practiceSet2;
+
+public class ps2question1 {
+    public static void main(String[] args) {
+        float a = 7/4.0f * 9/2.0f;
+        System.out.println(a);
+    }
+}
