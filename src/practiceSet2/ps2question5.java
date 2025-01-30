19bd1a59ac7fd42793dcb7b3cfc2c3bb708a0391
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-01-30 21:28:33 +0530

    edit ps2question5.java file

---
+++ src/practiceSet2/ps2question5.java
@@ -0,0 +1,10 @@
+//find the value of the following expression: int x = 7;  int a = 7*49/7+35/7; value of a?
+package practiceSet2;
+
+public class ps2question5 {
+    public static void main(String[] args) {
+
+        int a = 7*49/7+35/7;
+        System.out.println(a);
+    }
+}
