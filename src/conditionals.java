6d26001c44bb6b9a881526cdcbe23b4cf3291279
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-04 18:26:45 +0530

    Add conditionals.java file

---
+++ src/conditionals.java
@@ -0,0 +1,11 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int age = 26;
+        if (age>18) {
+            System.out.println("yes you can drive!");
+        }
+        else {
+            System.out.println("no you can not drive !");
+        }
+    }
+}
