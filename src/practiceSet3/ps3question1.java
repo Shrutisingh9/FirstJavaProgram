d8a0f240cc7ebdae09f45c98b2fd95bf74f6f6bb
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-04 17:54:16 +0530

    edit ps3question1.java file

---
+++ src/practiceSet3/ps3question1.java
@@ -0,0 +1,9 @@
+//wap to convert a java program to convert a string to lowerCase
+package practiceSet3;
+
+public class ps3question1 {
+    public static void main(String[] args) {
+        String name= "TANYA";
+        System.out.println(name.toLowerCase());
+    }
+}
