7ad3abb304fe21b9c299daea989b018a374fad16
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-04 17:57:00 +0530

    edit ps3question2.java file

---
+++ src/practiceSet3/ps3question2.java
@@ -0,0 +1,9 @@
+//wap to replace spaces with underscores.
+package practiceSet3;
+
+public class ps3question2 {
+    public static void main(String[] args) {
+        String name = "My name is Shruti";
+        System.out.println(name.replace(" ", "_"));
+    }
+}
