abe7db38823015a23333af88344c86f0303f2b90
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-04 18:06:55 +0530

    edit ps3question3.java file

--- src/practiceSet3/ps3question3.java
+++ src/practiceSet3/ps3question3.java
@@ -1,10 +1,12 @@
 /*wap to fill in a letter template which looks like
- letter = "Dear <|name|> , Thanks a lot"*/
+ letter = "Dear <|name|> , Thanks a lot"
+ Replace <|name|> with a string (some name)*/
 package practiceSet3;
 
 public class ps3question3 {
     public static void main(String[] args) {
-        String name = "Shruti";
-        System.out.println("Dear " +name+", Thanks a lot");
+        String letter = "Dear <|name|> , Thanks a lot";
+        letter = letter.replace ("<|name|>" , "Shruti");
+        System.out.println(letter);
     }
 }
