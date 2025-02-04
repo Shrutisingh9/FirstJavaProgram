74bcd103cba0c989486473150eae72e4cbca9687
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-04 18:17:03 +0530

    Add ps3question5.java file

---
+++ src/practiceSet3/ps3question5.java
@@ -0,0 +1,11 @@
+/*
+wap to format the following letter using escape sequence characters
+letter = "Dear Harry, This Java Course id=s nice.Thanks"
+ */
+package practiceSet3;
+
+public class ps3question5 {
+    public static void main(String[] args) {
+        System.out.println("\"Dear Harry, This Java Course is nice.Thanks\"");
+    }
+}
