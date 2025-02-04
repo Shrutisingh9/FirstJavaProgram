a5577bec9fb2ab53382643460dfa4dc8deeae852
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-04 18:02:19 +0530

    edit ps3question3.java file

---
+++ src/practiceSet3/ps3question3.java
@@ -0,0 +1,10 @@
+/*wap to fill in a letter template which looks like
+ letter = "Dear <|name|> , Thanks a lot"*/
+package practiceSet3;
+
+public class ps3question3 {
+    public static void main(String[] args) {
+        String name = "Shruti";
+        System.out.println("Dear " +name+", Thanks a lot");
+    }
+}
