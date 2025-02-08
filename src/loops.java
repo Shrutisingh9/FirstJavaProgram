b874e838f9d4b45a4d8c5fe89e4a6630aa81d274
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-08 15:29:17 +0530

    Add loops.java file

---
+++ src/loops.java
@@ -0,0 +1,17 @@
+public class loops {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+
+        System.out.println("Using Loops");
+        int i = 1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running While Loop!");
+
+    }
+}
