ca2c04a7a1d0ec9cc979f66a5d0eb0d077de6b12
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-02-13 20:49:09 +0530

    Add arrays.java file

---
+++ src/arrays.java
@@ -0,0 +1,20 @@
+public class arrays {
+    public static void main(String[] args) {
+        //Classroom of 500 students - You have to store marks of these 500 students
+        //Three main ways to write an array
+        //1. declaration and then memory allocation
+//        int[] marks;
+//        marks = new int[5];
+        //2.Declaration+memory allocation together
+//        int[] marks = new int [5];
+        //initialization
+//        marks[0] = 100;
+//        marks[1] = 70;
+//        marks[2] = 80;
+//        marks[3] = 60;
+//        marks[4] = 79;
+        //3.Declaration +memory allocation+ initialization together
+        int[] marks = {95,58,79,80,98};
+        System.out.println(marks[4]);
+    }
+}
